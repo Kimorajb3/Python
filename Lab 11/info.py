5bceba645cbdafcb5acd12ebf3e557b5ff40c22b
Kimorajb3 <100797001+Kimorajb3@users.noreply.github.com>
2025-01-03 13:54:39 -0500

    Update info.py

--- Lab 11/info.py
+++ Lab 11/info.py
@@ -1,18 +1,3 @@
-#------------------------------------------------------------------
-# Class: CIST 2742 Beginning Python Programming
-# Term: Fall 2022
-# Instructor: Jianmin Wang
-# Description: Solution to Lab 11.1 Display Name and Address
-# Due: 4/25/2022
-# author Kimora Bailey
-# version 1.0
-#
-# By turning in this code, I Pledge:
-#  1. That I have completed the programming assignment independently.
-#  2. I have not copied the code from a student or any source.
-#  3. I have not given my code to any student.
-#
-#---------------------------------------------------------------------
 import tkinter
 
 class Mygui:
