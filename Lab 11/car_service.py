18f4df6d1aee49896593c31ebaf50db3166ae7a8
Kimorajb3 <100797001+Kimorajb3@users.noreply.github.com>
2025-01-03 13:54:22 -0500

    Update car_service.py

--- Lab 11/car_service.py
+++ Lab 11/car_service.py
@@ -1,18 +1,3 @@
-#------------------------------------------------------------------
-# Class: CIST 2742 Beginning Python Programming
-# Term: Fall 2022
-# Instructor: Jianmin Wang
-# Description: Solution to Lab 11.2 Lee's Automotive Service
-# Due: 4/25/2022
-# author Kimora Bailey
-# version 1.0
-#
-# By turning in this code, I Pledge:
-#  1. That I have completed the programming assignment independently.
-#  2. I have not copied the code from a student or any source.
-#  3. I have not given my code to any student.
-#
-#---------------------------------------------------------------------
 import tkinter
 
 class AutoServices:
