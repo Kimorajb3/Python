2229895ae9d3174f5a9f83c58b73bde718d630e4
Kimorajb3 <100797001+Kimorajb3@users.noreply.github.com>
2025-01-03 13:58:23 -0500

    Update divisible_by_56.py

--- Lab 3/divisible_by_56.py
+++ Lab 3/divisible_by_56.py
@@ -1,18 +1,3 @@
-#------------------------------------------------------------------
- # Class: CIST 2742 Beginning Python Programming
- # Term: Fall 2022
- # Instructor: Jianmin Wang
- # Description: Solution to Lab 3.1 Divisible by 5 and 6
- # Due: 2/14/2022
- # author Kimora Bailey
- # version 1.0
- #
- # By turning in this code, I Pledge:
- #  1. That I have completed the programming assignment independently.
- #  2. I have not copied the code from a student or any source.
- #  3. I have not given my code to any student.
- #
- #---------------------------------------------------------------------
 print("Numbers divisble by 5 or 6 are: ")
 for i in range(1,101):
     if i % 5 == 0 or i % 6 == 0:
