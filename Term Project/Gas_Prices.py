1a3f31a8e82128496f96f7f07367b1837d8b1f60
Kimorajb3 <100797001+Kimorajb3@users.noreply.github.com>
2025-01-03 14:09:20 -0500

    Update Gas_Prices.py

--- Term Project/Gas_Prices.py
+++ Term Project/Gas_Prices.py
@@ -1,18 +1,3 @@
-#------------------------------------------------------------------
-# Class: CIST 2742 Beginning Python Programming
-# Term: Fall 2022
-# Instructor: Jianmin Wang
-# Description: Solution to Term Project(1): Statistics of Gas Prices
-# Due: 5/03/2022
-# author Kimora Bailey
-# version 1.0
-#
-# By turning in this code, I Pledge:
-#  1. That I have completed the programming assignment independently.
-#  2. I have not copied the code from a student or any source.
-#  3. I have not given my code to any student.
-#
-#---------------------------------------------------------------------
 from datetime import datetime
 
 
