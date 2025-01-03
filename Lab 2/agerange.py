3f17c179fdb047cf7e913956ba891bc59c1d2c69
Kimorajb3 <100797001+Kimorajb3@users.noreply.github.com>
2025-01-03 13:56:01 -0500

    Update agerange.py

--- Lab 2/agerange.py
+++ Lab 2/agerange.py
@@ -1,18 +1,3 @@
-#------------------------------------------------------------------
- # Class: CIST 2742 Beginning Python Programming
- # Term: Fall 2022
- # Instructor: Jianmin Wang
- # Description: Solution to Lab 2.3 age range
- # Due: 2/07/2022
- # author Kimora Bailey
- # version 1.0
- #
- # By turning in this code, I Pledge:
- #  1. That I have completed the programming assignment independently.
- #  2. I have not copied the code from a student or any source.
- #  3. I have not given my code to any student.
- #
- #---------------------------------------------------------------------
 age = int(input("Enter age: "))
 
 if age < 20 or age >= 50:
