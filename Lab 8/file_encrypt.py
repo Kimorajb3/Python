c2dabad7b74c4f9ce7e0b410090d65e01591fc4e
Kimorajb3 <100797001+Kimorajb3@users.noreply.github.com>
2025-01-03 14:04:31 -0500

    Update file_encrypt.py

--- Lab 8/file_encrypt.py
+++ Lab 8/file_encrypt.py
@@ -1,18 +1,3 @@
-#------------------------------------------------------------------
- # Class: CIST 2742 Beginning Python Programming
- # Term: Fall 2022
- # Instructor: Jianmin Wang
- # Description: Solution to Lab 8.1 File Encryption
- # Due: 3/28/2022
- # author Kimora Bailey
- # version 1.0
- #
- # By turning in this code, I Pledge:
- #  1. That I have completed the programming assignment independently.
- #  2. I have not copied the code from a student or any source.
- #  3. I have not given my code to any student.
- #
- #---------------------------------------------------------------------
 def en(codes):
     File = open('C:\\Users\\bellk\\OneDrive\\Documents\\Lab 8\\data.txt.txt', 'r')
     Read = File.read()
