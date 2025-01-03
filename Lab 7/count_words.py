1446c044212d6296532cb74d670512e8604f782f
Kimorajb3 <100797001+Kimorajb3@users.noreply.github.com>
2025-01-03 14:03:20 -0500

    Update count_words.py

--- Lab 7/count_words.py
+++ Lab 7/count_words.py
@@ -1,18 +1,3 @@
-#------------------------------------------------------------------
- # Class: CIST 2742 Beginning Python Programming
- # Term: Fall 2022
- # Instructor: Jianmin Wang
- # Description: Solution to Lab 7.1 Count words
- # Due: 3/21/2022
- # author Kimora Bailey
- # version 1.0
- #
- # By turning in this code, I Pledge:
- #  1. That I have completed the programming assignment independently.
- #  2. I have not copied the code from a student or any source.
- #  3. I have not given my code to any student.
- #
- #---------------------------------------------------------------------
 sent=[]
 W=0
 tf=open("text.txt",'r')   
